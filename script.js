9b34a6470b3822e4447038d58aef2c81998d9943
Aurelian <aurelianstanica30@gmail.com>
2025-03-25 18:25:06 +0000

    I checked html and css files with the W£C validation service and adjusted the images by adding alt to all of them and correcting 2 mistakes on css file. Added script.js file and started working on it

---
+++ script.js
@@ -0,0 +1,27 @@
+const waffle = document.getElementById("waffle");
+const creme = document.getElementById("cremeBrulee");
+const addToCart = document.getElementById("add-to-cart");
+
+
+
+// function add() {
+//     let adding = document.createElement("BUTTON");
+//     let aha = document.createTextNode("click me");
+//     adding.appendChild(aha);
+//     document.body.appendChild(adding);
+// }
+
+// add();
+
+
+
+
+
+function add() {
+ 
+   
+    addToCart.textContent = "one";
+    
+}
+
+waffle.addEventListener('click', add);
