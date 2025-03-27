e2b78740c8c5b8db28e35f136939e3018efbfca3
Aurelian <aurelianstanica30@gmail.com>
2025-03-27 16:32:12 +0000

    changes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const waffle = document.getElementById("waffle");
 const creme = document.getElementById("cremeBrulee");
-const addToCart = document.getElementById("add-to-cart");
 
+const addToCart = document.getElementById("add-to-cart");
+let total = 0;
+
+
+const productPrice = {
+    waffle: 6.50,
+    creme: 7.00,
+    macaron: 8.00,
+    tiramisu: 5.50,
+    baklava: 4.00,
+    pie: 5.00,
+    cake: 4.50, 
+    brownie: 4.50,
+    panna: 6.50
+}
 
 
 // function add() {
@@ -17,11 +31,16 @@ const addToCart = document.getElementById("add-to-cart");
 
 
 
-function add() {
- 
-   
-    addToCart.textContent = "one";
+function addPrices() {
+  
+  
+
     
 }
 
-waffle.addEventListener('click', add);
+
+for(let item of productPrice) {
+    total += productPrice.baklava * productPrice.value;
+}
+ 
+console.log("total cart" , total);
